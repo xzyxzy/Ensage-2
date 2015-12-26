7d1a98f4c39669d010af2b0f93f6fad72e5e45d4
spyware293 <faisalfiqhi@gmail.com>
2015-12-26 14:15:53 +0800

    Add farm mode and aghanim

--- Bristleback Sharp/Bristleback Sharp/Program.cs
+++ Bristleback Sharp/Bristleback Sharp/Program.cs
@@ -5,6 +5,7 @@ using Ensage.Common;
 using Ensage.Common.Extensions;
 using Ensage.Common.Menu;
 using SharpDX;
+using System.Windows.Input;
 
 namespace Bristleback_Sharp
 {
@@ -14,7 +15,7 @@ namespace Bristleback_Sharp
         private static Hero _source, _target;
         private static Item abyssal, solar, medallion, halberd, atos, dust;
         private static bool chase;
-        
+        private static readonly uint[] Quilldmg = { 20, 40, 60, 80 };
         private static readonly Menu Menu = new Menu("Bristleback", "bristle", true);
         static void Main(string[] args)
         {
@@ -22,7 +23,7 @@ namespace Bristleback_Sharp
             Game.OnWndProc += Game_OnWndProc;
             Game.PrintMessage("Bristleback Sharp by <font color='#ff1111'>Spyware293</font> Loaded !!", MessageType.LogMessage);
             var menu_utama = new Menu("Options", "opsi");
-            menu_utama.AddItem(new MenuItem("Quill", "Quill").SetValue(new StringList(new[] { "Max", "Smart", "Disable" })));
+            menu_utama.AddItem(new MenuItem("Quill", "Quill").SetValue(new StringList(new[] { "Max", "Smart", "Disable","Farm" })));
             menu_utama.AddItem(new MenuItem("enable", "enable").SetValue(true));
             Menu.AddSubMenu(menu_utama);
             Menu.AddToMainMenu();
@@ -48,7 +49,7 @@ namespace Bristleback_Sharp
         public static void Game_OnUpdate(EventArgs args)
         {
             _source = ObjectMgr.LocalHero;
-            var _enemy = ObjectMgr.GetEntities<Hero>().Where(hero => hero.IsAlive && !hero.IsIllusion && hero.IsVisible && hero.Team != _source.Team);
+           
             if (!Game.IsInGame||Game.IsPaused||Game.IsWatchingGame)
             {
                 return;
@@ -57,6 +58,8 @@ namespace Bristleback_Sharp
             {
                 return;
             }
+            var _enemy = ObjectMgr.GetEntities<Hero>().Where(hero => hero.IsAlive && !hero.IsIllusion && hero.IsVisible && hero.Team != _source.Team);
+            var _creep = ObjectMgr.GetEntities<Creep>().Where(x => (x.ClassID == ClassID.CDOTA_BaseNPC_Creep_Lane || x.ClassID == ClassID.CDOTA_BaseNPC_Creep_Siege) && x.IsAlive && x.IsSpawned && x.IsVisible).ToList();
             if (_source == null)
             {
                 return;
@@ -92,6 +95,18 @@ namespace Bristleback_Sharp
             if (halberd == null)
             {
                 halberd = _source.FindItem("item_heavens_halberd");
+            }
+            if (Menu.Item("Quill").GetValue<StringList>().SelectedIndex == 3 && Quill.CanBeCasted() && _source.CanCast() && Utils.SleepCheck("quill") && !_source.IsChanneling() && !_source.IsInvisible())
+            {
+                
+                foreach (var x in _creep)
+                {
+                    if (x.Team == _source.GetEnemyTeam() && x.Health > 0 && x.Health < (Quilldmg[Quill.Level-1] * (1-x.DamageResist)+20) && GetDistance2D(x.Position, _source.Position) < Quill.CastRange && Utils.SleepCheck("quill"))
+                    {
+                        Quill.UseAbility();
+                        Utils.Sleep(150 + Game.Ping, "quill");
+                    }
+                }
             }
             if (Menu.Item("Quill").GetValue<StringList>().SelectedIndex == 0 && Quill.CanBeCasted() && _source.CanCast() && Utils.SleepCheck("quill") && !_source.IsChanneling() && !_source.IsInvisible())
             {
@@ -147,8 +162,20 @@ namespace Bristleback_Sharp
                     }
                     if (Goo.CanBeCasted() && _source.CanAttack() && !_target.IsInvul() && Utils.SleepCheck("Goo"))
                     {
-                        Goo.UseAbility(_target);
-                        Utils.Sleep(150 + Game.Ping, "Goo");
+                        if (_source.HasItem(ClassID.CDOTA_Item_UltimateScepter))
+                        {
+                            if (GetDistance2D(_target.Position, _source.Position) < Goo.CastRange)
+                            {
+                                Goo.UseAbility();
+                                Utils.Sleep(150 + Game.Ping, "Goo");
+                            }
+
+                        }
+                        else
+                        {
+                            Goo.UseAbility(_target);
+                            Utils.Sleep(150 + Game.Ping, "Goo");
+                        }
                     }
                     if (!Goo.CanBeCasted() && Utils.SleepCheck("animationatk"))
                     {
